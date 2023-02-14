c9d324ff8cb810c5f5c9f647ad58b4c7de395e52
mulhern <amulhern@redhat.com>
2023-02-13 21:48:32 -0500

    Add test_harness.py to run test scripts
    Signed-off-by: mulhern <amulhern@redhat.com>

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 .PHONY: lint
 lint:
+	pylint test_harness.py
 	pylint stratis_cli_cert.py
 	pylint stratisd_cert.py
 	pylint testlib
@@ -7,12 +8,20 @@ lint:
 
 .PHONY: fmt
 fmt:
-	isort stratis_cli_cert.py stratisd_cert.py testlib scripts
+	isort \
+	test_harness.py \
+	stratis_cli_cert.py \
+	stratisd_cert.py \
+	testlib scripts
 	black .
 
 .PHONY: fmt-travis
 fmt-travis:
-	isort --diff --check-only stratis_cli_cert.py stratisd_cert.py testlib scripts
+	isort --diff --check-only \
+	test_harness.py \
+	stratis_cli_cert.py \
+	stratisd_cert.py \
+	testlib scripts
 	black . --check
 
 .PHONY: yamllint
---
+++ test_harness.py
@@ -0,0 +1,115 @@
+# Copyright 2023 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Simple test harness for running test scripts on loopbacked devices.
+"""
+
+# isort: STDLIB
+import argparse
+import itertools
+import os
+import subprocess
+import tempfile
+
+_LOSETUP_BIN = "/usr/sbin/losetup"
+_SIZE_OF_DEVICE = 1024**4  # 1 TiB
+
+
+def _make_loopbacked_devices(num):
+    """
+    Make the requisite number of loopbacked devices.
+
+    :param int num: number of devices
+    """
+
+    tdir = tempfile.mkdtemp("_stratis_test_loopback")
+
+    devices = []
+    for index in range(num):
+        backing_file = os.path.join(tdir, f"block_device_{index}")
+
+        with open(backing_file, "ab") as dev:
+            dev.truncate(_SIZE_OF_DEVICE)
+
+        device = str.strip(
+            subprocess.check_output(
+                [_LOSETUP_BIN, "-f", "--show", backing_file]
+            ).decode("utf-8")
+        )
+
+        devices.append(device)
+
+    return devices
+
+
+def _run_command(num_devices, command):
+    """
+    Prepare devices and run command on devices.
+
+    :param int num_devices: number of loopbacked devices
+    :param list command: the command to be run
+    """
+    devices = _make_loopbacked_devices(num_devices)
+    command = command + list(itertools.chain(*[["--disk", dev] for dev in devices]))
+    subprocess.run(command, check=True)
+
+
+def _run_stratisd_cert(_namespace):
+    _run_command(3, ["python3", "stratisd_cert.py", "--monitor-dbus", "-v"])
+
+
+def _run_stratis_cli_cert(_namespace):
+    _run_command(3, ["python3", "stratis_cli_cert.py", "-v"])
+
+
+def _gen_parser():
+    """
+    Generate the parser.
+    """
+    parser = argparse.ArgumentParser(
+        description=(
+            "Run specified test script on loopbacked devices. This script is "
+            "intended to be run on a disposable testing machine, it does not "
+            "clean up resources, such as temporary directories."
+        )
+    )
+
+    subparsers = parser.add_subparsers(title="subcommand")
+
+    stratisd_cert_parser = subparsers.add_parser(
+        "stratisd_cert", help="Run stratisd_cert.py"
+    )
+    stratisd_cert_parser.set_defaults(func=_run_stratisd_cert)
+
+    stratis_cli_cert_parser = subparsers.add_parser(
+        "stratis_cli_cert", help="Run stratis_cli_cert.py"
+    )
+    stratis_cli_cert_parser.set_defaults(func=_run_stratis_cli_cert)
+
+    return parser
+
+
+def main():
+    """
+    The main method.
+    """
+    parser = _gen_parser()
+
+    namespace = parser.parse_args()
+
+    namespace.func(namespace)
+
+
+if __name__ == "__main__":
+    main()
