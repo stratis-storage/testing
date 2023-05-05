3712e35d213cb6c3e0e9fad4fbfbc4f50d2d68e1
mulhern <amulhern@redhat.com>
2023-05-05 12:23:00 -0400

    Revert "Revert "Update revision to r6""
    This reverts commit c25c06592b25265f68279940ce5204e95ee938e7.

--- testlib/dbus.py
+++ testlib/dbus.py
@@ -96,7 +96,7 @@ class StratisDbus:
     _BUS = dbus.SystemBus()
     _BUS_NAME = "org.storage.stratis3"
     _TOP_OBJECT = "/org/storage/stratis3"
-    REVISION_NUMBER = 5
+    REVISION_NUMBER = 6
     _REVISION = f"r{REVISION_NUMBER}"
     BUS_NAME = _BUS_NAME
     TOP_OBJECT = _TOP_OBJECT
