889663c79c6c4c808edcaf563041d0b386dabf27
mulhern <amulhern@redhat.com>
2024-03-06 10:47:03 -0500

    Use regular expression to specify interfaces to check
    Signed-off-by: mulhern <amulhern@redhat.com>
    (cherry picked from commit 2682a39ba0183f525c7bed2ab57c058a7563cd6b)

--- scripts/monitor_dbus_signals.py
+++ scripts/monitor_dbus_signals.py
@@ -18,6 +18,7 @@ Monitor D-Bus properties and signals and verify that the signals are correct
 with respect to their properties.
 """
 
+_INTERFACE_RE = None
 _MO = None
 _SERVICE = None
 _TOP_OBJECT = None
@@ -41,6 +42,7 @@ try:
     # isort: STDLIB
     import argparse
     import os
+    import re
     import sys
     import time
     import xml.etree.ElementTree as ET
@@ -170,6 +172,10 @@ try:
         :param str object_path: D-Bus object path
         :param dict interfaces_added: map of interfaces to D-Bus properties
         """
+        interfaces_added = {
+            k: v for k, v in interfaces_added.items() if re.fullmatch(_INTERFACE_RE, k)
+        }
+
         if object_path == _TOP_OBJECT_PATH:
             interfaces_added = {
                 k: v for k, v in interfaces_added.items() if k in _TOP_OBJECT_INTERFACES
@@ -202,6 +208,10 @@ try:
         :param str object_path: D-Bus object path
         :param list interfaces: list of interfaces removed
         """
+        interfaces = {
+            k: v for k, v in interfaces.items() if re.fullmatch(_INTERFACE_RE, k)
+        }
+
         if object_path == _TOP_OBJECT_PATH:
             interfaces = {
                 k: v for k, v in interfaces.items() if k in _TOP_OBJECT_INTERFACES
@@ -248,6 +258,9 @@ try:
                 return
 
             interface_name = props_changed[0]
+            if not re.fullmatch(_INTERFACE_RE, interface_name):
+                return
+
             properties_changed = props_changed[1]
             properties_invalidated = props_changed[2]
 
@@ -297,7 +310,7 @@ try:
         except Exception as exc:  # pylint: disable=broad-except
             _CALLBACK_ERRORS.append(exc)
 
-    def _monitor(service, manager, manager_interfaces):
+    def _monitor(service, manager, manager_interfaces, interface_re):
         """
         Monitor the signals and properties of the manager object.
 
@@ -305,15 +318,18 @@ try:
         :param str manager: object path that of the ObjectManager implementor
         :param manager_interfaces: list of manager interfaces
         :type manager_interfaces: list of str
+        :param interface_re: regular expression to match interfaces to check
+        :type interface_re: re.Pattern
         """
 
-        global _TOP_OBJECT, _TOP_OBJECT_PATH, _TOP_OBJECT_INTERFACES, _SERVICE, _MO  # pylint: disable=global-statement
+        global _TOP_OBJECT, _TOP_OBJECT_PATH, _TOP_OBJECT_INTERFACES, _SERVICE, _MO, _INTERFACE_RE  # pylint: disable=global-statement
 
         dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
         bus = dbus.SystemBus()
         _SERVICE = service
         _TOP_OBJECT_PATH = manager
         _TOP_OBJECT_INTERFACES = manager_interfaces
+        _INTERFACE_RE = interface_re
 
         while True:
             try:
@@ -387,6 +403,13 @@ try:
             help="interface belonging to the top object",
         )
 
+        parser.add_argument(
+            "--only-check",
+            default=".*",
+            type=re.compile,
+            help="regular expression that restricts interfaces to check",
+        )
+
         return parser
 
     def main():
@@ -398,7 +421,7 @@ try:
 
         args = parser.parse_args()
 
-        _monitor(args.service, args.manager, args.top_interface)
+        _monitor(args.service, args.manager, args.top_interface, args.only_check)
 
     if __name__ == "__main__":
         main()
