7e9f4edd86284069b39c73ae253a2623f56982d8
Bryan Gurney <bgurney@redhat.com>
2024-05-09 15:20:26 -0400

    stratis_cli_cert: add test for pool debug get-metadata
    Signed-off-by: Bryan Gurney <bgurney@redhat.com>

--- stratis_cli_cert.py
+++ stratis_cli_cert.py
@@ -479,6 +479,19 @@ class StratisCliCertify(
         make_test_pool(StratisCliCertify.DISKS[0:1])
         self._test_permissions([_STRATIS_CLI, "pool", "list"], False, False)
 
+    @skip(_skip_condition(1))
+    def test_pool_debug_get_metadata(self):
+        """
+        Test running "stratis pool debug get-metadata" on a pool.
+        """
+        pool_name = make_test_pool(StratisCliCertify.DISKS[0:1])
+        self._unittest_command(
+            [_STRATIS_CLI, "pool", "debug", "get-metadata", f"--name={pool_name}"],
+            0,
+            True,
+            False,
+        )
+
     def test_blockdev_list(self):
         """
         Test listing a blockdev.
