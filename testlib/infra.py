0e072b57a1514f24f5a76fa05f9c694b28e4b1ca
mulhern <amulhern@redhat.com>
2024-12-11 10:54:56 -0500

    Use stratisd-tools stratis-checkmetadata
    Use this executable to verify properties of the Stratis pool-level
    metadata.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- testlib/infra.py
+++ testlib/infra.py
@@ -258,6 +258,8 @@ class PoolMetadataMonitor(unittest.TestCase):
 
         :param int stop_time: the time the test completed
         """
+        stratisd_tools = "stratisd-tools"
+
         if PoolMetadataMonitor.verify:  # pylint: disable=no-member
 
             # Wait for D-Bus to settle, so D-Bus and metadata can be compared
@@ -281,6 +283,25 @@ class PoolMetadataMonitor(unittest.TestCase):
                     )
 
                     self._check_encryption_information_consistency(object_path, written)
+
+                    with NamedTemporaryFile(mode="w") as temp_file:
+                        temp_file.write(json.dumps(written))
+                        temp_file.flush()
+
+                        try:
+                            with subprocess.Popen(
+                                [
+                                    stratisd_tools,
+                                    "stratis-checkmetadata",
+                                    temp_file.name,
+                                ],
+                                stdout=subprocess.PIPE,
+                            ) as proc:
+                                (stdoutdata, _) = proc.communicate()
+                                self.assertEqual(proc.returncode, 0, stdoutdata)
+                        except FileNotFoundError as err:
+                            raise RuntimeError(f"{stratisd_tools} not found") from err
+
                 else:
                     current_message = (
                         "" if current_return_code == _OK else current_message
