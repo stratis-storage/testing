b1f8f535678d180e70350ccae10812da9010034e
mulhern <amulhern@redhat.com>
2025-01-03 09:00:15 -0500

    Add additional arguments for pool creation
    Signed-off-by: mulhern <amulhern@redhat.com>

--- testlib/dbus.py
+++ testlib/dbus.py
@@ -327,6 +327,9 @@ class StratisDbus:
             devices,
             (True, key_desc) if key_desc is not None else (False, ""),
             (True, clevis_info) if clevis_info is not None else (False, ("", "")),
+            (False, 0),
+            (False, ""),
+            (False, 0),
             timeout=StratisDbus._TIMEOUT,
         )
 
